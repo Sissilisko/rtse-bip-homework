8734ccd6188168ff50d17b5168d39b89b77b0a84
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-21 22:37:04 +0200

    Ex 12 done

--- part1/variables/exercise_12/Program.cs
+++ part1/variables/exercise_12/Program.cs
@@ -7,6 +7,15 @@ namespace exercise_12
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number!");
+
+      string userInput = Console.ReadLine();
+
+      Console.WriteLine("You gave " + userInput);
+
+
+
+
 
     }
   }
