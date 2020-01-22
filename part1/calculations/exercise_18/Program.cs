9c1b70d836146ba1735cc77b14cfb5c5c595ca1f
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-22 22:54:55 +0200

    Ex 18 done

--- part1/calculations/exercise_18/Program.cs
+++ part1/calculations/exercise_18/Program.cs
@@ -8,7 +8,22 @@ namespace exercise_18
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int frst = Convert.ToInt32(frstNmbr);
+      
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int scnd = Convert.ToInt32(scndNmbr);
 
+      Console.WriteLine("Give the third number!");
+      string thrdNmbr = Console.ReadLine();
+      int thrd = Convert.ToInt32(thrdNmbr);
+
+      Console.WriteLine("The sum is " + (frst + scnd + thrd));
+
+
+      
     }
   }
 }
