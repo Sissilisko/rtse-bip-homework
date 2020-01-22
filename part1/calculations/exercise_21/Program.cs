c1730befdd0827ce95398818929c07625ccb7616
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-23 00:29:14 +0200

    Ex 21 done

--- part1/calculations/exercise_21/Program.cs
+++ part1/calculations/exercise_21/Program.cs
@@ -8,6 +8,16 @@ namespace exercise_21
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int frst = Convert.ToInt32(frstNmbr);
+
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int scnd = Convert.ToInt32(scndNmbr);
+      
+      double average = (frst + scnd) / 2.0;
+      Console.WriteLine("The average is " + average);
 
     }
   }
