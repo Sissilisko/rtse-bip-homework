dcd203cd78a85f148abca9701c28089a82fadf4c
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-23 00:40:53 +0200

    Ex 22 done

--- part1/calculations/exercise_22/Program.cs
+++ part1/calculations/exercise_22/Program.cs
@@ -8,6 +8,20 @@ namespace exercise_22
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int frst = Convert.ToInt32(frstNmbr);
+
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int scnd = Convert.ToInt32(scndNmbr);
+
+      Console.WriteLine("Give the third number!");
+      string thrdNmbr = Console.ReadLine();
+      int thrd = Convert.ToInt32(thrdNmbr);
+
+      double average = (frst + scnd + thrd) / 3.0;
+      Console.WriteLine("The average is " + average);
 
     }
   }
