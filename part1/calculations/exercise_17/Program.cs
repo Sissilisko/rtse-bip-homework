5582534bedfde52eb9e36b931b234427034d1474
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-22 22:33:04 +0200

    Ex 17 done

--- part1/calculations/exercise_17/Program.cs
+++ part1/calculations/exercise_17/Program.cs
@@ -8,6 +8,17 @@ namespace exercise_17
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int frst = Convert.ToInt32(frstNmbr);
+
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int scnd = Convert.ToInt32(scndNmbr);
+
+      Console.WriteLine("The sum is " + (frst + scnd)); 
+
+
 
     }
   }
