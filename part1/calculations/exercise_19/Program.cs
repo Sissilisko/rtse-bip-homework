bc0e410c044ccd58b8b5423ad6f802ab7ec1ea15
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-22 23:04:43 +0200

    ex 19 done

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -8,6 +8,15 @@ namespace exercise_19
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int frst = Convert.ToInt32(frstNmbr);
+
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int scnd = Convert.ToInt32(scndNmbr);
+
+      Console.WriteLine(frst + " + " + scnd + " = " + (frst + scnd));
 
     }
   }
