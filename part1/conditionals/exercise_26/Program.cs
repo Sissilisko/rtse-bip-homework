f26989fc9f4324fb16dcc13caf15ccd32f53ae58
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-23 23:56:43 +0200

    Ex 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -7,6 +7,13 @@ namespace exercise_26
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give your age:");
+      string userAge = Console.ReadLine();
+      int intAge  = Convert.ToInt32(userAge);
+      if (intAge <1900)
+      {
+        Console.WriteLine("You're old");
+      }
 
     }
   }
