01698e01423947239dee216a1335e916f50a40ef
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-23 23:51:14 +0200

    Ex 25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -7,6 +7,14 @@ namespace exercise_25
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string askNmbr = Console.ReadLine();
+      int year = Convert.ToInt32(askNmbr);
+      if ( year == 1984)
+      {
+        Console.WriteLine("Orwell");
+      }
+
 
     }
   }
