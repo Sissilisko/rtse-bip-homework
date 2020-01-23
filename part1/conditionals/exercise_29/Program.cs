7eb4df8b8eb2a412112793fb12c26e6dc300dda7
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-24 00:56:21 +0200

    Ex 29 done

--- part1/conditionals/exercise_29/Program.cs
+++ part1/conditionals/exercise_29/Program.cs
@@ -7,6 +7,27 @@ namespace exercise_29
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      string frstNmbr = Console.ReadLine();
+      int intFrst = Convert.ToInt32(frstNmbr);
+      
+      Console.WriteLine("Give the second number!");
+      string scndNmbr = Console.ReadLine();
+      int intScnd = Convert.ToInt32(scndNmbr);
+      
+      if (intFrst > intScnd)
+      {
+        Console.WriteLine("The larger number is " + intFrst + "!");
+      }
+      else if (intFrst < intScnd)
+      {
+        Console.WriteLine("The larger number is " + intScnd + "!");
+      }
+      else
+      {
+        Console.WriteLine("They are equal!");
+      }
+
 
     }
   }
