8da466385c17cb21c8637c1b31d34def1d7318b8
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-24 00:28:57 +0200

    Ex 27 done

--- part1/conditionals/exercise_27/Program.cs
+++ part1/conditionals/exercise_27/Program.cs
@@ -7,6 +7,17 @@ namespace exercise_27
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string userNmbr = Console.ReadLine();
+      int intNmbr = Convert.ToInt32(userNmbr);
+      if (intNmbr > 0)
+      {
+        Console.WriteLine("It is positive");
+      }
+      else
+      {
+      Console.WriteLine("It is not positive");
+      }
 
     }
   }
