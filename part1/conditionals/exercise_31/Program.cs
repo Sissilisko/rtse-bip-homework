4164fb97916f035a943117638b17235bb813334b
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 00:37:59 +0200

    Ex 31 done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_31
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give a number:");
+      string usrNmbr = Console.ReadLine();
+      int intNmbr = Convert.ToInt32(usrNmbr);
+
+      if (intNmbr % 2 == 0)
+      {
+        Console.WriteLine("It is even.");
+      }
+      else
+      {
+        Console.WriteLine("It is odd.");
+      }
 
     }
   }
