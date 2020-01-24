bfe15142ccd629fa7b46677b89da349da18950a8
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 00:51:57 +0200

    Ex 32 done

--- part1/conditionals/exercise_32/Program.cs
+++ part1/conditionals/exercise_32/Program.cs
@@ -7,6 +7,16 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Speak, friend, and enter!");
+      string entryAnswer = Console.ReadLine();
+      if (entryAnswer == "Mellon")
+      {
+        Console.WriteLine("Welcome, friend!");
+      }
+      else
+      {
+       Console.WriteLine("They've got a cave troll!");
+      }
 
     }
   }
