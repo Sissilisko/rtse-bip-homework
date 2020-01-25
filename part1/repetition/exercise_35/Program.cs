7f58ec8f6d71c5e2daccbacce5f7b85e1a4ff3d6
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 13:37:21 +0200

    Ex 35 done

--- part1/repetition/exercise_35/Program.cs
+++ part1/repetition/exercise_35/Program.cs
@@ -7,6 +7,16 @@ namespace exercise_35
     public static void Main(string[] args)
     {
       // Write your code here:
+      while(true)
+      {
+        Console.WriteLine("Give a number:");
+        string giveNmbr = Console.ReadLine();
+        int nmbr = Convert.ToInt32(giveNmbr);
+        if (nmbr == 42)
+        {
+          break;
+        }
+      }
 
     }
   }
