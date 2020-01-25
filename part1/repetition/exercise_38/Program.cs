4088ac250231180a91fc6f201f99cad66a5ea79d
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 15:57:50 +0200

    Ex 38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,6 +7,21 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
+      int negNmbr = 0;
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int giveNmbr = Convert.ToInt32(Console.ReadLine());
+        if (giveNmbr == 0)
+        {
+          break;
+        }
+        if (giveNmbr < 0)
+        {
+          negNmbr++; 
+        }
+      }
+      Console.WriteLine("Total amount of negative numbers: " + negNmbr);
 
     }
   }
