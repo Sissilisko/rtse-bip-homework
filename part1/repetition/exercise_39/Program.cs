f9c7ea0beb25269eb171d7e738556240016a9ada
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 16:27:48 +0200

    ex 39 done

--- part1/repetition/exercise_39/Program.cs
+++ part1/repetition/exercise_39/Program.cs
@@ -7,6 +7,21 @@ namespace exercise_39
     public static void Main(string[] args)
     {
       // Write your code here:
+      int sum = 0;
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int giveNmbr = Convert.ToInt32(Console.ReadLine());
+        if (giveNmbr == 0)
+        {
+          break;
+        }
+        else
+        {
+          sum = sum + giveNmbr;
+        }
+      }
+      Console.WriteLine("Total sum of numbers: " + sum);
     }
   }
 }
