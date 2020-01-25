b18057c1d61ad85b305e34c387df598e25aec8c8
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-25 13:23:35 +0200

    Ex 33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -7,6 +7,18 @@ namespace exercise_33
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Give the first string:");
+      string frstInput = Console.ReadLine();
+      Console.WriteLine("Give the second string:");
+      string scndInput = Console.ReadLine();
+      if (frstInput == scndInput)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
 
     }
   }
