a998425c5009933f95eb9fde56ea13301636f95f
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-28 00:42:50 +0200

    Ex 44 done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+      int frstInt = Convert.ToInt32(Console.ReadLine());
+      int scndInt = Convert.ToInt32(Console.ReadLine());
+      if (frstInt > scndInt)
+      {
+        Console.WriteLine(frstInt + " is greater than " + scndInt + ".");
+      }
+      else if (frstInt < scndInt)
+      {
+        Console.WriteLine(frstInt + " is less than " + scndInt + ".");
+      }
+      else
+      {
+        Console.WriteLine(frstInt + " is equal to " + scndInt + ".");
+      }
 
     }
   }
