d11f21888a7d4fbc587eb97e058d5071c77d3a8b
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-30 01:07:07 +0200

    Ex 48 s2

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -7,8 +7,19 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      // Write your code here:
- 
+      int sum = 0;
+      Console.WriteLine("Give numbers:");
+      while (true)
+      {
+        int giveNmbr = Convert.ToInt32(Console.ReadLine());
+        if (giveNmbr == -1)
+        {
+          break;
+        }
+        sum += giveNmbr;
+      }
+      Console.WriteLine("Thx! Bye!");
+      Console.WriteLine("Sum: " + sum);
     }
   }
 }
