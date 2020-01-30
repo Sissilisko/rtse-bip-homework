89ec3486ec618180081fe7bcba67b44ea88667be
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-30 20:32:36 +0200

    Ex 51 done

--- part2/methods/exercise_51/Program.cs
+++ part2/methods/exercise_51/Program.cs
@@ -7,11 +7,19 @@ namespace exercise_51
     public static void Main(String[] args)
     {
       // Call your method here:
-
-
+      PrintUntilNumber(3);
     }
 
     // Write your method here:
+    public static void PrintUntilNumber(int number)
+    {
+      int count = 1;
+      while(count <= number)
+      {
+        Console.WriteLine(count);
+        count++;
+      }
+    }
 
   }
 }
