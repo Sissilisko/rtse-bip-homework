4b4d8479c857a106f408b56d492d5e40c134e8de
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-30 17:14:44 +0200

    Ex 48 done

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -7,8 +7,11 @@ namespace exercise_48
     public static void Main(string[] args)
     {
 
-      int sum = 0;
       Console.WriteLine("Give numbers:");
+      int sum = 0;
+      int count = 0;
+      int even = 0;
+      int odd = 0;
       while (true)
       {
         int giveNmbr = Convert.ToInt32(Console.ReadLine());
@@ -17,9 +20,23 @@ namespace exercise_48
           break;
         }
         sum += giveNmbr;
+        count++;
+        if ((giveNmbr % 2) == 0)
+        {
+          even++;
+        }
+        else
+        {
+          odd++;
+        }  
       }
       Console.WriteLine("Thx! Bye!");
       Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + count);
+      Console.WriteLine("Average: " + (double)sum / count);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
+      
     }
   }
 }
