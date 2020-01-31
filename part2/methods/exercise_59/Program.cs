afd7e0bcf938b02aecc500339ebbc7d412585f2d
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-31 19:46:33 +0200

    Ex 59 done

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -11,6 +11,23 @@ namespace exercise_59
     }
 
     // Write your method here:
+    public static int Greatest(int nmbr1, int nmbr2, int nmbr3)
+    {
+      int answer = 0;
+      if (nmbr1 > nmbr2 & nmbr1 > nmbr3)
+      {
+        answer = nmbr1;
+      }
+      else if (nmbr2 > nmbr1 & nmbr2 > nmbr3)
+      {
+        answer = nmbr2;
+      }
+      else
+      {
+        answer = nmbr3;
+      }
+      return answer;
+    }
 
   }
 }
