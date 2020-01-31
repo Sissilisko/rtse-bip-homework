c21572d0d29831386efbdc0430f79192e5902fca
Anna Isokoski <isokoski.anna@gmail.com>
2020-01-31 23:43:19 +0200

    Ex 61 done

--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,27 +6,57 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+      PrintSpaces(0);
+      PrintStars(0);
+      PrintRightTriangle(0);
+      ChristmasTree(10);
     }
 
-    public static void PrintStars(int number)
+    public static void PrintStars(int stars)
     {
-
+      for (int i = 0; i < stars; i++)
+      {
+        Console.Write("*");
+      }
+      Console.WriteLine("");
     }
 
-    public static void PrintSpaces(int number)
+    public static void PrintSpaces(int space)
     {
-
+      for (int i = 0; i < space; i++)
+      {
+        Console.Write(" ");
+      }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+      int left = size -1;
+      for (int i = 1; i <= size; i++)
+      {
+        PrintSpaces(left);
+        PrintStars(i);
+        left--;
+      }
     }
 
     public static void ChristmasTree(int height)
     {
-
+      int left = height -1;
+      int starsRow = 1;
+      for (int i = 1; i <= height; i++)
+      {
+        PrintSpaces(left);
+        PrintStars(starsRow);
+        starsRow += 2;
+        left--;
+      }
+      int treeFoot = height - 2;
+      for (int x = 0; x < 2; x++)
+      {
+        PrintSpaces(treeFoot);
+        PrintStars(3);
+      }
     }
   }
 }
