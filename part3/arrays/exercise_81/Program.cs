12d1bf68625bdc6e2a4e79e9bb01a2be3a30e8b6
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-09 23:44:43 +0200

    Ex 81 done

--- part3/arrays/exercise_81/Program.cs
+++ part3/arrays/exercise_81/Program.cs
@@ -13,7 +13,17 @@ namespace exercise_81
 
     public static void PrintNeatly(int[] array)
     {
-
+      for (int i = 0; i < array.Length; i++)
+      {
+        if (i < array.Length - 1)
+        {
+          Console.Write(array[i] + ", ");
+        }
+        else
+        {
+          Console.Write(array[i] + "\n");
+        }
+      }
     }
   }
 }
