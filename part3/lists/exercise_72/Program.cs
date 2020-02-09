70d01cbfa8eaf21cf6197037a6be05c6744ff5f0
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-09 19:06:15 +0200

    Ex 72 done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 
 namespace exercise_72
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-
+        public static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == 9999)
+                {
+                    break;
+                }
+                list.Add(input);
+            }
+            int small = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (small > list[i])
+                {
+                    small = list[i];
+                }
+            }
+            Console.WriteLine("Smallest number: " + small);
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == small)
+                    Console.WriteLine("Found at index: " + i);
+            }
+        }
     }
-  }
 }
