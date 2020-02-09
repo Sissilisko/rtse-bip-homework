f1b1ba0295fa6d8c2400357ff1bef13944aaf0d5
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-09 15:45:53 +0200

    Ex 71 done

--- part3/lists/exercise_71/Program.cs
+++ part3/lists/exercise_71/Program.cs
@@ -17,7 +17,15 @@ namespace exercise_71
         }
         list.Add(input);
       }
-
+      Console.WriteLine("Search for?");
+      int srchFor = Convert.ToInt32(Console.ReadLine());
+      for (int i = 0; i < list.Count; i++)
+      {
+        if (list[i] == srchFor)
+        {
+          Console.WriteLine (srchFor + " is at index " +i);
+        }
+      }
     }
   }
 }
