090ecc97ff69f480b0f81a2067a934025927c368
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-10 18:12:16 +0200

    Ex 87 done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
-
+      while (true)
+      {
+        string usrInput = Console.ReadLine();
+        if (usrInput == "")
+        {
+          break;
+        }
+        string[] pieces = usrInput.Split(" ");
+        foreach (string line in pieces)
+        {
+          if (line.Contains("av"))
+          {
+          Console.WriteLine(line);
+          }
+        }
+      }
     }
 
   }
