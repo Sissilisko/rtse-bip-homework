45c3729b888f13d25e7fd0750f4ef658fb293c8e
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-11 01:19:03 +0200

    Ex 90 done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -7,8 +7,21 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
-
-
+      int oldest = 0;
+      while (true)
+      {
+        string usrInput = Console.ReadLine();
+        if (usrInput == "")
+        {
+          break;
+        }
+        string[] pieces = usrInput.Split(",");
+        if (oldest < Convert.ToInt32(pieces[1]))
+        {
+          oldest = Convert.ToInt32(pieces[1]);
+        }
+      }
+      Console.WriteLine("Age of the oldest: " + oldest);
     }
   }
 }
