2ece1720c43f06f32b3e3e20ff6a557e12a0b729
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-16 01:19:22 +0200

    Ex 93 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,7 +8,9 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
-
+      Account shadyBusinesses = new Account("Shady Businesses", 100.00);
+      shadyBusinesses.Deposit(20.0);
+      Console.WriteLine(shadyBusinesses.balance);
     }
   }
 }
