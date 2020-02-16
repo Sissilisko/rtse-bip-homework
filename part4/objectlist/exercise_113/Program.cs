97a50c56d5a0b4d24fc789f7bc2cea08c7b124b0
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-16 23:52:23 +0200

    Ex 113 done

--- part4/objectlist/exercise_113/Program.cs
+++ part4/objectlist/exercise_113/Program.cs
@@ -9,7 +9,43 @@ namespace exercise_113
     {
       // IMPLEMENT YOUR CODE IN HERE!
       // Remember also to create the file Book.cs and class in it!
+      List<Book> list = new List<Book>();
+      while (true)
+      {
+        Console.Write("Name: ");
+        String name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        
+        Console.Write("Pages: ");
+        int pages = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Publication year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+       
+        list.Add(new Book(name, pages, year));
+      }
+      Console.WriteLine();
+      Console.Write("What information will be printed? ");
+      string printed = Console.ReadLine();
 
+      foreach (Book item in list)
+      {
+        if (printed == "title")
+        {
+          Console.WriteLine(item.name);
+        }
+        else if (printed == "everything")
+        {
+          Console.WriteLine(item);
+        }
+        else
+        {
+          break;
+        }
+      }
     }
   }
 }
