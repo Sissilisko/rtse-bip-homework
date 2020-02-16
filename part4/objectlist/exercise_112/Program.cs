915bf3aef6e9ccbe944c5adb63f0c95fb3425db5
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-16 23:17:48 +0200

    Ex 112 done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -8,8 +8,29 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
-
-
+      List<TelevisionProgram> list = new List<TelevisionProgram>();
+      while (true)
+      {
+        Console.Write("Name: ");
+        String name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        Console.Write("Duration: ");
+        int duration = Convert.ToInt32(Console.ReadLine());
+        list.Add(new TelevisionProgram(name, duration));
+      }
+      Console.WriteLine();
+      Console.Write("Program's maximum duration? ");
+      int maxDur = Convert.ToInt32(Console.ReadLine());
+      foreach(TelevisionProgram item in list)
+      {
+        if (item.duration <= maxDur)
+        {
+          Console.WriteLine(item);
+        }
+      }
     }
   }
 }
