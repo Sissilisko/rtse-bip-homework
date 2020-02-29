f759488866b9c0fdd18b070d09b4d13b5bc4a863
Anna Isokoski <isokoski.anna@gmail.com>
2020-02-29 22:15:06 +0200

    Ex 126 done

--- part5/references/exercise_126/Program.cs
+++ part5/references/exercise_126/Program.cs
@@ -2,11 +2,23 @@
 
 namespace exercise_126
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Try your code here, if you want
+        public static void Main(string[] args)
+        {
+            // Try your code here, if you want
+            PaymentTerminal lunchCafeteria = new PaymentTerminal();
+
+            double change = lunchCafeteria.DrinkCoffee(0);
+            Console.WriteLine("remaining change " + change);
+
+            change = lunchCafeteria.DrinkCoffee(2);
+            Console.WriteLine("remaining change " + change);
+
+            change = lunchCafeteria.EatLunch(10);
+            Console.WriteLine("remaining change " + change);
+
+            Console.WriteLine(lunchCafeteria);
+        }
     }
-  }
 }
